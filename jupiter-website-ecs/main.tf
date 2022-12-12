977f91f44b347b98b6502629dd8ee85609b3eb05
jhoel100 <49628552+jhoel100@users.noreply.github.com>
2022-12-12 08:35:47 -0500

    Add files via upload

---
+++ jupiter-website-ecs/main.tf
@@ -0,0 +1,44 @@
+#congirure aws provider
+provider "aws" {
+  region  = var.region
+  profile = "terraform-user"
+}
+
+#create vpc
+
+module "vpc" {
+  source = "../modules/vpc"
+  region = var.region 
+  project_name = var.project_name
+  vpc_cdir = var.vpc_cdir
+  public_subnet_az1_cidr = var.public_subnet_az1_cidr
+  public_subnet_az2_cidr = var.public_subnet_az2_cidr
+  private_app_subnet_az1_cidr = var.private_app_subnet_az1_cidr
+  private_app_subnet_az2_cidr = var.private_app_subnet_az2_cidr
+  private_data_subnet_az1_cidr = var.private_data_subnet_az1_cidr
+  private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
+
+}
+
+module "nat_gateway" {
+  source = "../modules/nat-gateway"
+  public_subnet_az1_id= module.vpc.public_subnet_az1_id
+  internet_gateway= module.vpc.internet_gateway
+  public_subnet_az2_id= module.vpc.public_subnet_az2_id
+  vpc_id= module.vpc.vpc_id
+  private_app_subnet_az1_id= module.vpc.private_app_subnet_az1_id
+  private_data_subnet_az1_id= module.vpc.private_data_subnet_az1_id
+  private_app_subnet_az2_id= module.vpc.private_app_subnet_az2_id
+  private_data_subnet_az2_id= module.vpc.private_data_subnet_az2_id
+}
+
+
+module "application_load_balancer" {
+  source = "../modules/alb"
+  project_name = module.vpc.project_name
+  alb_security_group_id = module.security_group.alb_security_group_id
+  public_subnet_az1_id = module.vpc.public_subnet_az1_id
+  public_subnet_az2_id = module.vpc.public_subnet_az2_id
+  vpc_id = module.vpc.vpc_id
+  certificate_arn = module.acm.certificate_arn
+}
